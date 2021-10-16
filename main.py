659a5f2234ba22458189700d60cc9e08fe6d8f7b
Jámin Johnson <menthol54@protonmail.com>
2021-10-15 20:55:50 -0400

    adding color to text and beignning of story

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import story as s
 import os
 import time
-from slowprint import slowprint
+import story as s
+from termcolor import colored
 
 
 def intros():
@@ -12,8 +14,9 @@ def intros():
         intro_2 = input("Would you like a hear story? (Yes) or (No): ")
         if intro_2 == 'yes' or intro_2 == 'Yes':
             time.sleep(1)
-            slowprint("Very Good. Welcome to Theoritical Judgement.", 0.1)
+            print("Very Good. Welcome to Theoritical Judgement.")
             return True
+            os.system('cls')
         elif intro_2 == 'No' or intro_2 == 'no':
             print("Use Control + C to end the game. So long... user.")
         else:
@@ -26,4 +29,9 @@ def intros():
         return intros()
 
 
-intros()
+def story():
+    print(s.d_intro[0])
+    print(colored("What is your name?", 'green'))
+
+
+story()
