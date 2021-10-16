ecd7600f300956f1413371a86504dbb2c51ebb47
Jámin Johnson <menthol54@protonmail.com>
2021-10-16 13:50:28 -0400

    def intros() is completed(for now)

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ from termcolor import colored
 import sys
 os.system('color')
 
+global rejection, running
+
+rejection = ("A simple Yes or No would be appreciated.")
+
+running = False
 
 def print_slow(str):
     for char in str:
@@ -17,28 +22,28 @@ def intros():
     os.system('cls')
     time.sleep(1)
     intro_1 = input('Would you like to play a game? (Yes) or (No): ')
-    rejection = ("A simple Yes or No would be appreciated.")
-    if intro_1 == 'yes' or intro_1 == 'Yes':
+    if intro_1.lower() == 'yes':
         intro_2 = input("Would you like a hear story? (Yes) or (No): ")
-        if intro_2 == 'yes' or intro_2 == 'Yes':
+        if intro_2.lower() == 'yes':
             time.sleep(1)
             print("Very Good. Welcome to Theoritical Judgement.")
-            return True
+            time.sleep(0.5)
             os.system('cls')
-        elif intro_2 == 'No' or intro_2 == 'no':
+            return True
+        elif intro_2.lower() == 'no':
             print("Use Control + C to end the game. So long... user.")
         else:
             print(rejection)
-            return
-    elif intro_1 == 'No' or intro_1 == 'no':
+            time.sleep(1)
+            intros()
+    elif intro_1.lower() == 'no':
         print("Use Control + C to end the game. So long... user.")
     else:
         print(rejection)
-        return intros()
-
+        time.sleep(1)
+        intros()
 
 def intro_dialogue():
+    os.system('cls')
     print_slow(colored(d_intro[0], 'red'))
 
-
-intro_dialogue()
