4ec16985e9cb46517e709e052b92428d57412a71
Katsuo <70943621+Kiyoshi-bit@users.noreply.github.com>
2021-10-19 10:11:03 -0500

    added a class for decisions

--- classes.py
+++ classes.py
@@ -2,3 +2,8 @@ class Decision:
     def __init__(self, prompt, choices):
         self.prompt = prompt
         self.choices = choices
+
+    def choice_amount(self):
+        k = 0
+        for x in self.choices:
+            k += 1
