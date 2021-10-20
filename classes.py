63edcba14252885d9651f204a9ff5e2220c97d3e
Jámin Johnson <menthol54@protonmail.com>
2021-10-20 10:36:06 -0400

    skills

--- classes.py
+++ classes.py
@@ -1,9 +1,10 @@
 class Player:
-    def __init__(self, persuasion, spirit, maturity, pride, love, choice):
+    def __init__(self, persuasion, spirit, maturity, pride, love, choice, level):
         self.persuasion = persuasion
         self.spirit = spirit
         self.maturity = maturity
         self.pride = pride
         self.love = love
         self.choice = choice
+        self.levels = level 
 
