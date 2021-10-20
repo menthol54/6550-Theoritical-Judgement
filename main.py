a10cf13c6fb26579ed5335ba738f68d540f9ac78
Jámin Johnson <menthol54@protonmail.com>
2021-10-20 10:04:28 -0400

    additons to monologue

--- main.py
+++ main.py
@@ -11,16 +11,26 @@ running = False
 
 def print_slow(str):
     for char in str:
-        time.sleep(.09)
+        time.sleep(.05)
         sys.stdout.write(char)
         sys.stdout.flush()
 def main():
     if s.start() == True:
         running = True
     if running == True:
-        time.sleep(2)
-        s.intro_1()
-        time.sleep(2)
-        s.intro_2()
+        try:
+            time.sleep(2)
+            s.intro_1()
+            time.sleep(2)
+            s.intro_2()
+            time.sleep(2)
+            s.intro_3()
+            time.sleep(2)
+            s.intro_4()
+            time.sleep(2)
+            s.intro_5()
+        except KeyboardInterrupt:
+            os.system('cls')
+            os.system('color')
         
 if __name__ == '__main__': main()
--- story.py
+++ story.py
@@ -13,30 +13,30 @@ rejection = ("A simple Yes or No would be appreciated.")
 ent = input('Enter to continue: ')
 
 def start():
-    os.system('cls')
-    time.sleep(1)
-    intro_1 = input('Would you like to play a game? (Yes) or (No): ')
-    if intro_1.lower() == 'yes':
-        intro_2 = input("Would you like a hear story? (Yes) or (No): ")
-        if intro_2.lower() == 'yes':
-            time.sleep(1)
-            print("Very Good. Welcome to Theoritical Judgement.")
-            time.sleep(2)
-            print_slow(colored(ent, 'white'))
-            os.system('cls')
-            return True
-        elif intro_2.lower() == 'no':
+        os.system('cls')
+        time.sleep(1)
+        intro_1 = input('Would you like to play a game? (Yes) or (No): ')
+        if intro_1.lower() == 'yes':
+            intro_2 = input("Would you like a hear story? (Yes) or (No): ")
+            if intro_2.lower() == 'yes':
+                time.sleep(1)
+                print("Very Good. Welcome to Theoritical Judgement.")
+                time.sleep(2)
+                print_slow(colored(ent, 'white'))
+                os.system('cls')
+                return True
+            elif intro_2.lower() == 'no':
+                print("Use Control + C to end the game. So long... user.")
+            else:
+                print(rejection)
+                time.sleep(1)
+                start()
+        elif intro_1.lower() == 'no':
             print("Use Control + C to end the game. So long... user.")
         else:
             print(rejection)
             time.sleep(1)
             start()
-    elif intro_1.lower() == 'no':
-        print("Use Control + C to end the game. So long... user.")
-    else:
-        print(rejection)
-        time.sleep(1)
-        start()
 
 def intro_1():
     os.system('cls')
@@ -52,6 +52,7 @@ def intro_1():
     time.sleep(0.5)
     print_slow(colored(opening_1[5], 'red'))
     time.sleep(0.5)
+    print_slow(colored(ent, 'white'))
 def intro_2():
     os.system('cls')
     os.system('color')
@@ -61,5 +62,37 @@ def intro_2():
     print_slow(colored(opening_2[2], 'red'))
     time.sleep(0.5)
     print_slow(colored(opening_2[3], 'red'))
-    ent
-    
+    print_slow(colored(ent, 'white'))
+def intro_3():
+        os.system('cls')
+        os.system('color')
+        print_slow(colored(opening_3[0], 'red'))
+        print_slow(colored(opening_3[1], 'red'))
+        time.sleep(0.5)
+        print_slow(colored(opening_3[2], 'red'))
+        time.sleep(0.5)
+        print_slow(colored(opening_3[3], 'red'))
+        time.sleep(0.5)
+        print_slow(colored(opening_3[4], 'red'))
+        print_slow(colored(opening_3[5], 'red'))
+        time.sleep(0.5)
+        print_slow(colored(opening_3[6], 'red'))
+        print_slow(colored(ent, 'white'))
+def intro_4():
+    os.system('cls')
+    os.system('color')
+    print_slow(colored(opening_4[0], 'red'))  
+    time.sleep(0.5)
+    print_slow(colored(opening_4[1], 'red'))
+    time.sleep(0.5)
+    print_slow(colored(opening_4[2], 'red'))
+    time.sleep(0.5)
+def intro_5():
+    os.system('cls')
+    os.system('color')
+    print_slow(colored(opening_5[0], 'red'))  
+    time.sleep(0.5)
+    print_slow(colored(opening_5[1], 'red'))
+    time.sleep(0.5)
+    print_slow(colored(opening_5[2], 'red'))
+    time.sleep(0.5)
