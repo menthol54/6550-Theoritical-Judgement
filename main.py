930a9626a866b89e0674c5b96240e3b4fc82969e
Katsuo <70943621+Kiyoshi-bit@users.noreply.github.com>
2021-10-20 20:37:23 -0500

    added save functionality

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from story import *
 import os
 import time
 from termcolor import colored
@@ -5,32 +6,64 @@ import sys
 import story as s
 os.system('color')
 
-rejection = ("A simple Yes or No would be appreciated.")
+rejection = "A simple Yes or No would be appreciated."
 
 running = False
 
-def print_slow(str):
-    for char in str:
+
+def print_slow(string):
+    for char in string:
         time.sleep(.05)
         sys.stdout.write(char)
         sys.stdout.flush()
+
+
 def main():
-    if s.start() == True:
+    global running
+    global act
+    if s.start():
         running = True
-    if running == True:
+    if running:
         try:
-            time.sleep(2)
-            s.intro_1()
-            time.sleep(2)
-            s.intro_2()
-            time.sleep(2)
-            s.intro_3()
-            time.sleep(2)
-            s.intro_4()
-            time.sleep(2)
-            s.intro_5()
+            if act == '1':
+                time.sleep(2)
+                s.intro_1()
+                time.sleep(2)
+                s.intro_2()
+                time.sleep(2)
+                s.intro_3()
+                time.sleep(2)
+                s.intro_4()
+                time.sleep(2)
+                s.intro_5()
+            elif act == '2':
+                time.sleep(2)
+                s.intro_2()
+                time.sleep(2)
+                s.intro_3()
+                time.sleep(2)
+                s.intro_4()
+                time.sleep(2)
+                s.intro_5()
+            elif act == '3':
+                time.sleep(2)
+                s.intro_3()
+                time.sleep(2)
+                s.intro_4()
+                time.sleep(2)
+                s.intro_5()
+            elif act == '4':
+                time.sleep(2)
+                s.intro_4()
+                time.sleep(2)
+                s.intro_5()
+            elif act == '5':
+                time.sleep(2)
+                s.intro_5()
         except KeyboardInterrupt:
             os.system('cls')
             os.system('color')
-        
-if __name__ == '__main__': main()
+
+
+if __name__ == '__main__':
+    main()
