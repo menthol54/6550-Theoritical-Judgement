dace4080815e787b95964aa880fab77a1b423510
Katsuo <70943621+Kiyoshi-bit@users.noreply.github.com>
2021-10-22 16:02:03 -0500

    improved the run functionality

--- main.py
+++ main.py
@@ -25,42 +25,9 @@ def main():
             running = True
             from story import act
         if running:
-            if act == '1':
-                for x in functions:
-                    time.sleep(2)
-                    x()
-            elif act == '2':
-                for x in functions[1:]:
-                    time.sleep(2)
-                    x()
-            elif act == '3':
-                for x in functions[2:]:
-                    time.sleep(2)
-                    x()
-            elif act == '4':
-                for x in functions[3:]:
-                    time.sleep(2)
-                    x()
-            elif act == '5':
-                for x in functions[4:]:
-                    time.sleep(2)
-                    x()
-            elif act == '6':
-                for x in functions[5:]:
-                    time.sleep(2)
-                    x()
-            elif act == '7':
-                for x in functions[6:]:
-                    time.sleep(2)
-                    x()
-            elif act == '8':
-                for x in functions[7:]:
-                    time.sleep(2)
-                    x()
-            elif act == '9':
-                for x in functions[8:]:
-                    time.sleep(2)
-                    x()
+            for x in functions[int(act) - 1:]:
+                time.sleep(2)
+                x()
     except KeyboardInterrupt:
         os.system('color')
         os.system('cls')
